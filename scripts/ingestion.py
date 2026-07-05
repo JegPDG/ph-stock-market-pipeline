742b99e972639080504bc5a92e5cca29a0061a6c
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2026-07-05 21:20:39 +0800

    Created the reference table for the Stocks

--- scripts/ingestion.py
+++ scripts/ingestion.py
@@ -6,10 +6,20 @@ import os
 # BDOUY, JBFCY, SM
 tickers = ["BDOUY", "JBFCY", "SM"]
 
+# Stock reference
+stocks_data = [
+    ("SM.PS", "SM Investments Corporation", "PSE", "Conglomerates", "PHP"),
+    ("JBFCY", "Jollibee Foods Corporation", "OTC", "Consumer Staples", "USD"),
+    ("BDOUY", "BDO Unibank Inc", "OTC", "Financials", "USD")
+]
+
+# Downloading the tickers
 df = yf.download(tickers, period="1mo", interval="1d")
 
+# This makes it into a clean table
 df_long = df.stack( level=1, future_stack=True).reset_index()
 
+#  Renaming the columns
 df_long = df_long.rename(
     columns={
         "Date": "trade_date",
@@ -24,9 +34,12 @@ df_long = df_long.rename(
 
 df_long.columns.name = None 
 
+# Crate an db engine
 conn = sqlite3.connect("sql_db/stocks.db")
+
 cursor = conn.cursor()
 
+# Saving to sql
 df_long.to_sql(
   "stocks_prices", 
   conn, 
@@ -44,11 +57,28 @@ for col in columns:
 
 
 # Create a reference table for the tickers 
+# 1 Create the table
+conn.execute("""
+    CREATE TABLE IF NOT EXISTS stocks (
+        ticker TEXT PRIMARY KEY,
+        name TEXT,
+        market TEXT,
+        sector TEXT,
+        currency TEXT
+    )
+""")
+
+conn.executemany("""
+    INSERT OR IGNORE INTO stocks (ticker, name, market, sector, currency)
+    VALUES (?, ?, ?, ?, ?)
+""", stocks_data)
+
+
 
 print(df_long)
 # df_from_db = pd.read_sql(f"SELECT * FROM stocks_prices", conn)
 # print(df_from_db)
-
+conn.commit()
 conn.close()
 
 
