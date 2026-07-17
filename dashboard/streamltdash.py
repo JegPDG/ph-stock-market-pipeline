03c13cd3ff2c1316cffaea108d9baf71f61e124a
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2026-07-17 22:46:53 +0800

    Created multiple charts Still creating more

--- dashboard/streamltdash.py
+++ dashboard/streamltdash.py
@@ -2,15 +2,23 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
+# Retrive daat from sql
 @st.cache_data
 def load_dashboard_data():
   with sqlite3.connect("sql_db/stocks.db") as conn:
     df_ag_tbl = pd.read_sql_query("SELECT ticker, avg_close, avg_high, avg_low, avg_open  FROM agregate_table", conn)
+  
+  with sqlite3.connect("sql_db/stocks.db") as conn:
+    df_stck_anly = pd.read_sql_query("SELECT * FROM stocks_analytics", conn)
 
-  return df_ag_tbl
+  with sqlite3.connect("sql_db/stocks.db") as conn:
+    df_stck_prc = pd.read_sql_query("SELECT * FROM stocks_prices", conn)
+  
+  return df_ag_tbl, df_stck_anly, df_stck_prc
 
-df_ag_tbl = load_dashboard_data()
+df_ag_tbl, df_stck_anly, df_stck_prc = load_dashboard_data()
 df_ag_tbl = df_ag_tbl.rename(columns={
   'avg_close':'Average Close',
   'avg_high':'Average High',
@@ -19,50 +27,127 @@ df_ag_tbl = df_ag_tbl.rename(columns={
 
 })
 
-# GROUPED BAR CHART 
-st.header("Stock Summary")
-
-# Group by Ticker
-df_long = df_ag_tbl.melt(
-  id_vars="ticker",
-  value_vars=['Average Close', 'Average High', 'Average Low', 'Average Open'],
-  var_name="Metric",
-  value_name="Price"
+# Ticker Dropdown
+ticker = st.selectbox(
+    "Select Stock",
+    df_stck_prc["ticker"].unique()
 )
 
-fig = px.bar(
-    df_long,
-    x="ticker",
-    y="Price",
-    color="Metric",
-    barmode="group",
-    title="Average Stock Prices by Ticker",
-    labels={
-        "ticker": "Stock",
-        "Price": "Price",
-        "Metric": "Price Type"
-    }
+
+# # GROUPED BAR CHART 
+# st.header("Stock Summary")
+
+# # Group by Ticker
+# df_long = df_ag_tbl.melt(
+#   id_vars="ticker",
+#   value_vars=['Average Close', 'Average High', 'Average Low', 'Average Open'],
+#   var_name="Metric",
+#   value_name="Price"
+# )
+
+# fig = px.bar(
+#     df_long,
+#     x="ticker",
+#     y="Price",
+#     color="Metric",
+#     barmode="group",
+#     title="Average Stock Prices by Ticker",
+#     labels={
+#         "ticker": "Stock",
+#         "Price": "Price",
+#         "Metric": "Price Type"
+#     }
+# )
+
+# st.plotly_chart(fig, use_container_width=True)
+
+# # Group by ticker
+# fig1 = px.bar(
+#     df_long,
+#     x="Metric",
+#     y="Price",
+#     color="ticker",
+#     barmode="group",
+#     title="Average Stock Prices by Metric",
+#     labels={
+#         "ticker": "Stock",
+#         "Price": "Price",
+#         "Metric": "Price Type"
+#     }
+# )
+
+# st.plotly_chart(fig1, use_container_width=True)
+
+
+# /////////////////////////////////////////
+
+st.title("Stock Candlestick Chart")
+
+df_stck_prc['trade_date'] = pd.to_datetime(df_stck_prc['trade_date'])
+df_stck_prc['trade_date'] = df_stck_prc['trade_date'].dt.strftime('%b %d')
+
+stock_df = df_stck_prc[df_stck_prc["ticker"] == ticker].sort_values("trade_date")
+
+fig2 = go.Figure(
+    data=[
+        go.Candlestick(
+            x=stock_df["trade_date"],
+            open=stock_df["open_price"],
+            high=stock_df["high_price"],
+            low=stock_df["low_price"],
+            close=stock_df["close_price"]
+        )
+    ]
 )
 
-st.plotly_chart(fig, use_container_width=True)
-
-# Group by ticker
-
-fig1 = px.bar(
-    df_long,
-    x="Metric",
-    y="Price",
-    color="ticker",
-    barmode="group",
-    title="Average Stock Prices by Metric",
-    labels={
-        "ticker": "Stock",
-        "Price": "Price",
-        "Metric": "Price Type"
-    }
+fig2.update_layout(
+    title=f"{ticker} Candlestick Chart",
+    xaxis_title="Date",
+    yaxis_title="Price",
+    xaxis_rangeslider_visible=False
 )
 
-st.plotly_chart(fig1, use_container_width=True)
+st.plotly_chart(fig2, use_container_width=True)
+
+# Volume Bar Chart per Ticker
+
+st.header("Volume Chart")
+st.bar_chart(
+  stock_df,
+  x="trade_date",
+  y='volume'
+)
 
 
-print (df_ag_tbl.info())
+# ////////////////////////////////////////
+df_stck_anly['trade_date'] = pd.to_datetime(df_stck_anly['trade_date'])
+df_stck_anly['trade_date'] = df_stck_anly['trade_date'].dt.strftime('%b %d')
+
+# Line chart for Close Price
+st.header("Close Price Comparison")
+st.line_chart(
+  df_stck_anly,
+  x= "trade_date",
+  y= "close_price",
+  color="ticker"
+)
+
+# Line chart for Volume 
+st.header("Volume Comparison")
+
+st.line_chart(
+  df_stck_anly,
+  x= "trade_date",
+  y= "volume",
+  color="ticker"
+)
+
+# Line chart for Volatility 
+st.header("Volatility Comparison")
+
+st.line_chart(
+  df_stck_anly,
+  x= "trade_date",
+  y= "volatility_7d",
+  color="ticker"
+)
