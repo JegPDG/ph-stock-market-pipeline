a6e9ea5ec78dbc21b267b58a9e6db9e6e4849d83
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2026-07-21 23:40:19 +0800

    Working on the dashboard

--- dashboard/streamltdash.py
+++ dashboard/streamltdash.py
@@ -8,22 +8,37 @@ import plotly.graph_objects as go
 @st.cache_data
 def load_dashboard_data():
   with sqlite3.connect("sql_db/stocks.db") as conn:
-    df_ag_tbl = pd.read_sql_query("SELECT ticker, avg_close, avg_high, avg_low, avg_open  FROM agregate_table", conn)
-  
-  with sqlite3.connect("sql_db/stocks.db") as conn:
-    df_stck_anly = pd.read_sql_query("SELECT * FROM stocks_analytics", conn)
-    df_stck_anly['trade_date'] = pd.to_datetime(df_stck_anly['trade_date']) # transform into datetime format
-    df_stck_anly = df_stck_anly.sort_values(by='trade_date', ascending=True) # sort ASC
-    df_stck_anly['trade_date'] = df_stck_anly['trade_date'].dt.strftime('%b %d') # Format to Month dd
+    df_ag_tbl = pd.read_sql_query(
+        "SELECT ticker, avg_close, avg_high, avg_low, avg_open FROM agregate_table",
+        conn
+    )
 
-  with sqlite3.connect("sql_db/stocks.db") as conn:
-    df_stck_prc = pd.read_sql_query("SELECT * FROM stocks_prices", conn)
-    df_stck_prc['trade_date'] = pd.to_datetime(df_stck_prc['trade_date'])
-    df_stck_prc['trade_date'] = df_stck_prc['trade_date'].dt.strftime('%b %d')
-  
-  return df_ag_tbl, df_stck_anly, df_stck_prc
+    df_stck_anly = pd.read_sql_query(
+        "SELECT * FROM stocks_analytics",
+        conn
+    )
 
-df_ag_tbl, df_stck_anly, df_stck_prc = load_dashboard_data()
+    df_stck_prc = pd.read_sql_query(
+        "SELECT * FROM stocks_prices",
+        conn
+    )
+
+    df_kpi = pd.read_sql_query(
+       """SELECT * 
+          FROM stocks_analytics
+          WHERE trade_date = (SELECT MAX(trade_date) FROM stocks_analytics)""",
+       conn
+    )
+
+    # Process dates
+    df_stck_anly["trade_date"] = pd.to_datetime(df_stck_anly["trade_date"])
+    df_stck_prc["trade_date"] = pd.to_datetime(df_stck_prc["trade_date"])
+    df_kpi["trade_date"] = pd.to_datetime(df_kpi["trade_date"])
+
+  
+  return df_ag_tbl, df_stck_anly, df_stck_prc, df_kpi
+
+df_ag_tbl, df_stck_anly, df_stck_prc, df_kpi = load_dashboard_data()
 
 # Rename columns
 df_ag_tbl = df_ag_tbl.rename(columns={
@@ -49,16 +64,39 @@ with col1:
       df_stck_prc["ticker"].unique()
   )
 
+  df_kpi_ticker = df_kpi[df_kpi['ticker'] == ticker]
+  df_kpi_ticker['trade_date'] = df_kpi_ticker['trade_date'].dt.strftime('%B %d, %Y')
+
+  kpis_dict = {
+     'current' : df_kpi_ticker['close_price'].values[0].round(2),
+     'daily_return' : df_kpi_ticker['daily_return'].values[0].round(2),
+     '7d_ma' : df_kpi_ticker['moving_avg_7d'].values[0].round(2),
+     '30d_ma' : df_kpi_ticker['moving_avg_30d'].values[0].round(2),
+     'today_volume' : df_kpi_ticker['volume'].values[0].round(2),
+     'volatility_7d' : df_kpi_ticker['volatility_7d'].values[0].round(2),
+      'trade_date' : df_kpi_ticker['trade_date'].values[0],  
+  }
+
   with st.container(border=True):
         st.markdown(
           f"""
-          <div style="font-size: 24px;">
-              KPI of {ticker}
+          <div style="font-size: 24px; display: flex; justify-content: space-between; align-content: center;">
+              <div> KPI of {ticker} </div>
+              <div style="font-size: 14px; color: #808080;"> {kpis_dict.get('trade_date')} </div>
           </div>
-          <div style="display: grid; grid-template-columns: 1fr 50px; font-size: 14px; padding: 0 20px 0px 20px;">
+          <div style="display: grid; grid-template-columns: 1fr 50px; font-size: 14px; padding: 10px 20px 10px 20px; gap: 14px;">
               <div> Current Price </div> 
-              <div> 1 peos </div> 
-              
+              <div> ₱{kpis_dict.get('current')} </div> 
+              <div> Daily Return </div> 
+              <div> {kpis_dict.get('daily_return')}% </div> 
+              <div> 7-day MA </div> 
+              <div> ₱{kpis_dict.get('7d_ma')} </div> 
+              <div> 30-day MA </div> 
+              <div> ₱{kpis_dict.get('30d_ma')} </div> 
+              <div> Today's Volume </div> 
+              <div> {kpis_dict.get('today_volume')}  </div> 
+              <div> Volatility (7D) </div> 
+              <div> {kpis_dict.get('volatility_7d')}%  </div> 
           </div>
 
           <div style="font-size: 24px;">
@@ -95,6 +133,37 @@ with col2:
   )
 
   st.plotly_chart(fig2, use_container_width=True)
+
+with col3:
+  with st.container(border=True):
+        st.markdown(
+          f"""
+          <div style="font-size: 24px; display: flex; justify-content: space-between; align-content: center;">
+              <div> Summary table for {ticker} </div>
+          </div>
+          <div style="display: grid; grid-template-columns: 1fr 50px; font-size: 14px; padding: 10px 20px 10px 20px; gap: 14px;">
+              <div> Current Price </div> 
+              <div> ₱{kpis_dict.get('current')} </div> 
+              <div> Daily Return </div> 
+              <div> {kpis_dict.get('daily_return')}% </div> 
+              <div> 7-day MA </div> 
+              <div> ₱{kpis_dict.get('7d_ma')} </div> 
+              <div> 30-day MA </div> 
+              <div> ₱{kpis_dict.get('30d_ma')} </div> 
+              <div> Today's Volume </div> 
+              <div> {kpis_dict.get('today_volume')}  </div> 
+              <div> Volatility (7D) </div> 
+              <div> {kpis_dict.get('volatility_7d')}%  </div> 
+          </div>
+
+          <div style="font-size: 24px;">
+          </div>
+          """, 
+          unsafe_allow_html=True
+      )
+
+
+
 
 # Volume Bar Chart per Ticker
 
